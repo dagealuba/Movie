da8ecefa89e8c8535bb4648b628f822495cf2b06
蒙荣珍 <1090644593@qq.com>
2019-07-04 15:00:22 +0800

    11

--- src/main/java/Service/UserService.java
+++ src/main/java/Service/UserService.java
@@ -1,9 +1,27 @@
 package Service;
 
 import Entity.User;
+import Entity.UserExample;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 public interface UserService {
 //    public int addUser(User user);
 
-    public User getUserById(String id);
+    int register(User user);
+
+    List<User> login(String username, String password);
+
+    List<User> findByName(@RequestParam String name);
+
+    List<User> findByEmail(@RequestParam String email);
+    //查找
+    User selectByPrimaryKey(String userid);
+
+    //通过条件删除
+    int deleteUserByName(String username);
+
+    //信息修改
+    int updateByPrimaryKey(User user);
 }
